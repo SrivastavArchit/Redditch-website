e2d9a4dd20a4acd3e340d1cdd38b922476ffce85
SrivastavArchit <architpulkit2003@gmail.com>
2024-03-11 15:13:06 +0530

    responsive navbar

--- js1/script.js
+++ js1/script.js
@@ -1,23 +1,43 @@
-// S// Selecting elements
+// Selecting elements
 let menu = document.querySelector(".menu"),
-sidebarOpen = document.querySelector(".sidebarOpen"),
-sidebarClose = document.querySelector(".sidebarClose");
+    sidebarOpen = document.querySelector(".sidebarOpen"),
+    sidebarClose = document.querySelector(".sidebarClose");
 
 // Function to hide the menu
 function hideMenu() {
-menu.style.display = "none";
+    menu.style.display = "none";
 }
+
+// Function to show the menu
+function showMenu() {
+    menu.style.display = "block";
+}
+
+// Function to toggle menu based on screen width
+function toggleMenu() {
+    if (window.innerWidth > 720) {
+        showMenu(); // Show menu on desktop screens
+    } else {
+        hideMenu(); // Hide menu on mobile screens
+    }
+}
+
+// Initial toggle based on screen width
+toggleMenu();
 
 // Toggle sidebar functionality
 sidebarOpen.addEventListener("click", () => {
-menu.style.display = "block";
+    showMenu();
 });
 
 sidebarClose.addEventListener("click", () => {
-hideMenu();
+    hideMenu();
 });
 
 // Event listener to hide the menu when navigating to a new page
-window.addEventListener("unload", () => {
-hideMenu();
+window.addEventListener("beforeunload", () => {
+    hideMenu();
 });
+
+// Event listener for window resize to toggle menu
+window.addEventListener("resize", toggleMenu);
